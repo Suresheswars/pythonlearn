54b32193a700c6b1e1a7594b5c3d6c9cf36c107f
Suresheswars <98255139+Suresheswars@users.noreply.github.com>
2026-01-28 13:51:15 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,52 @@
+#General variable:
+
+student = "raj"
+
+
+print(student)
+
+#Combining variables:
+
+python_engineer = "sooria"
+
+
+engineer =  "The python engineer is, " +python_engineer
+
+
+print(engineer)
+
+
+#Formatted String:
+
+#Example 1:
+
+python_engineer = "sooria"
+
+
+engineer =  f"The python engineer is, {python_engineer}"
+
+
+print(engineer)
+
+#Example 2:
+
+python_engineer = "sooria"
+linux_engineer = "ravi"
+
+
+engineer =  f"The python engineer is, {python_engineer} {linux_engineer}"
+
+
+print(engineer)
+
+
+#Getting inputs from Users:
+
+a = input("Enter the first name: ")
+b = input("Enter the second name: ")
+
+
+print(f"The AI_Ops engineer is: {a}")
+print(f"The ML_Ops engineer is: {b}")
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
